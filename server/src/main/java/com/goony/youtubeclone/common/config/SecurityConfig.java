7ec23c7c02cd8bf102774616bf8dd6332c8a4adf
Lee SangHyup <bosuksh@gmail.com>
2021-03-16 01:34:07 +0900

    feat: jwt filter and config 추가

--- server/src/main/java/com/goony/youtubeclone/common/config/SecurityConfig.java
+++ server/src/main/java/com/goony/youtubeclone/common/config/SecurityConfig.java
@@ -1,20 +1,39 @@
 package com.goony.youtubeclone.common.config;
 
+import com.goony.youtubeclone.common.jwt.JwtAuthenticationFilter;
+import com.goony.youtubeclone.common.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  private final JwtTokenProvider jwtTokenProvider;
+
+
+  @Bean
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
+
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
       .antMatchers(HttpMethod.POST,"/api/account").permitAll()
       .anyRequest().authenticated()
-    .and().httpBasic().disable();
+    .and().httpBasic().disable()
+    .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 
-    http.csrf().disable();
+    http.csrf().disable()
+    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
   }
 }
