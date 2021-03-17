8558b861152867adc536e2da077d215a5ba210e3
Lee SangHyup <bosuksh@gmail.com>
2021-03-18 00:37:39 +0900

    feat: password encoder해결 필요

--- server/src/main/java/com/goony/youtubeclone/common/config/SecurityConfig.java
+++ server/src/main/java/com/goony/youtubeclone/common/config/SecurityConfig.java
@@ -8,12 +8,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   private final JwtTokenProvider jwtTokenProvider;
@@ -28,7 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
-      .antMatchers(HttpMethod.POST,"/api/account").permitAll()
+      .antMatchers(HttpMethod.POST,"/api/signup","/api/auth").permitAll()
       .anyRequest().authenticated()
     .and().httpBasic().disable()
     .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
